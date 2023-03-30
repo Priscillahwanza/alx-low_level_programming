3b28aa0dbe46347607985d0d87d9272c8b745cbc
Priscillahwanza <priscillahwanza3@gmail.com>
2023-03-30 13:29:09 -0700

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int s;
+
+	for (s = 0; s < n && src[i] != '\0'; S++)
+	{
+		dest[s] = src[s];
+	}
+	dest[s] = '\0';
+	return (dest);
+}
